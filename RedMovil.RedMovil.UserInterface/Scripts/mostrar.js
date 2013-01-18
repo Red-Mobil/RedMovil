6842d2ef638afdaaa1314b8697bbf87572ff8458
Alan <alan.collante@gmail.com>
2013-01-18 19:08:51 -0300

    mapa agregado

--- RedMovil.RedMovil.UserInterface/Scripts/mostrar.js
+++ RedMovil.RedMovil.UserInterface/Scripts/mostrar.js
@@ -7,7 +7,31 @@ function mostrartodo() {
 	if (validarEmail(email))
 	{
 	document.getElementById("vista").innerHTML= '<center><input type="button" value="Enviar email" onclick="parent.location=\'mailto:'+email+'\'"><center></br><center><input type="button" value="Llamar" onclick="parent.location=\'tel:+56'+telefono+'\'"><center>';
+	var address = document.getElementById("address").value;
+	geocoder.geocode( { 'address': address}, function(results, status) {
+		if (status == google.maps.GeocoderStatus.OK) {
+			map.setCenter(results[0].geometry.location);
+			var marker = new google.maps.Marker({
+			map: map,
+			position: results[0].geometry.location
+			});
+		} else {
+			alert("Geocode was not successful for the following reason: " + status);
+		}
+	});
 	} else {
                 document.getElementById("vista").innerHTML= '<center><input type="button" value="Llamar" onclick="parent.location=\'tel:+56'+telefono+'\'"><center>';
+		var address = document.getElementById("address").value;
+		geocoder.geocode( { 'address': address}, function(results, status) {
+		if (status == google.maps.GeocoderStatus.OK) {
+			 map.setCenter(results[0].geometry.location);
+			var marker = new google.maps.Marker({
+			 map: map,
+			position: results[0].geometry.location
+			});
+		} else {
+			alert("Geocode was not successful for the following reason: " + status);
+		}
+		});
+	}
         }
-}
