56d23d637f5fd1dd5e65cbf68ff643c4f646c3bd
EDWARD SALAS <edwardsala@gmail.com>
2013-01-18 22:52:36 -0300

    se agregaron div para poder vizualizar el mapa dentro del telefono se modificaron dnd se vizualizan  Scripts/append.js Scripts/mapa.js

--- RedMovil.RedMovil.UserInterface/Scripts/append.js
+++ RedMovil.RedMovil.UserInterface/Scripts/append.js
@@ -5,11 +5,11 @@ $(document).ready(function(){
 		b =  "b".concat(id);
 		tt = "tt".concat(id);
 		bb = "bb".concat(id);
-		$("#editor").append("<textarea id="+t+" style='width:320px;height:100px'></textarea><br><textarea id="+b+" style='width:320px;height:100px'></textarea><br><br><br><br>");
-		$("#vista").append("<p id="+tt+"></p><br><p id="+bb+"></p><br><br><br><br>");
+		$("#texto").append("<textarea id="+t+" style='width:320px;'></textarea><br><textarea id="+b+" style='width:320px;'></textarea><br><br><br><br>");
+		$("#texto").append("<p id="+tt+"></p><br><p id="+bb+"></p><br><br><br><br>");
 		asd = new nicEditor({fullPanel : true}).panelInstance(b);
 		id++;
 		max -= 1;
 	}
   });
- }); 
+ });
