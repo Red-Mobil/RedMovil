ada11cb78e2f424b08a66f59a9aa153bc3ba5104
EDWARD SALAS <edwardsala@gmail.com>
2013-01-19 21:05:34 -0300

    RAUL AHI VA O QUE ME PEDISTE PARA QUE LE ECHES UN OJO

--- RedMovil.RedMovil.UserInterface/Scripts/append.js
+++ RedMovil.RedMovil.UserInterface/Scripts/append.js
@@ -5,8 +5,9 @@ $(document).ready(function(){
 		b =  "b".concat(id);
 		tt = "tt".concat(id);
 		bb = "bb".concat(id);
-		$("#editor").append("<textarea id="+t+" style='width:320px;'></textarea><br><textarea id="+b+" style='width:320px;'></textarea><br><br><br><br>");
-		$("#editor").append("<p id="+tt+"></p><br><p id="+bb+"></p><br><br><br><br>");
+		$("#editor").append("<textarea id="+t+" style='width:320px;'></textarea><textarea id="+b+" style='width:320px;'></textarea><br>");
+		$("#titulo").append("<p id="+tt+"></p>");
+		$("#parrafo").append("<p id="+bb+"></p>");
 		asd = new nicEditor({fullPanel : true}).panelInstance(b);
 		id++;
 		max -= 1;
