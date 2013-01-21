d3252ea963a432f1c8a26efaad0c01ae98acf690
Taufik <redmovil.dev@gmail.com>
2013-01-21 18:09:01 -0300

    asdasd

--- RedMovil.RedMovil.UserInterface/Scripts/append.js
+++ RedMovil.RedMovil.UserInterface/Scripts/append.js
@@ -43,7 +43,7 @@ $(document).ready(function () {
             b = "b".concat(id);
             tt = "tt".concat(id);
             bb = "bb".concat(id);
-            $("#texto").append("<textarea id=" + t + " style='width:320px;'>Titulo"+(id+1)+"</textarea><textarea id=" + b + " style='width:320px;'></textarea><br>");
+            $("#texto").append("<textarea id=" + t + " style='width:320px;' >Titulo"+(id+1)+"</textarea><textarea id=" + b + " style='width:320px;' ></textarea><br>");
             //$("#preview_vista_botones").append("<a href=#" + bb + "><button id=" + tt + " >Titulo"+(id+1)+"</button></a><br>");
 			$("#preview_vista_botones").append("<button id=" + tt + " onclick='mostrar("+bb+")' >Titulo"+(id+1)+"</button><br>");
             $("#preview_vista_texto").append("<div id=" + bb + "></div>");
