1c83fa010d8629d76fcda185464c79c12ba6ba6d
Taufik <redmovil.dev@gmail.com>
2013-01-21 14:56:10 -0300

    arreglado

--- RedMovil.RedMovil.UserInterface/Scripts/append.js
+++ RedMovil.RedMovil.UserInterface/Scripts/append.js
@@ -1,3 +1,11 @@
+	function showall()
+	{
+		document.getElementById('bb0').style.display = 'block';
+        document.getElementById('bb1').style.display = 'block';
+        document.getElementById('bb2').style.display = 'block';		
+	}
+	
+	
 	function hideall()
 	{
 		document.getElementById('bb0').style.display = 'none';
@@ -8,7 +16,7 @@
 	function mostrar(id)
 	{
 		hideall();
-		document.getElementById(id).style.display = 'block';
+		id.style.display = 'block';
 	}
 
 $(document).ready(function () {
@@ -29,8 +37,8 @@ $(document).ready(function () {
             tt = "tt".concat(id);
             bb = "bb".concat(id);
             $("#texto").append("<textarea id=" + t + " style='width:320px;'>Titulo"+(id+1)+"</textarea><textarea id=" + b + " style='width:320px;'></textarea><br>");
-            $("#preview_vista_botones").append("<a href=#" + bb + "><button id=" + tt + " >Titulo"+(id+1)+"</button></a><br>");
-			//$("#preview_vista_botones").append("<button id=" + tt + " onclick='mostrar("+bb+")' >Titulo"+(id+1)+"</button><br>");
+            //$("#preview_vista_botones").append("<a href=#" + bb + "><button id=" + tt + " >Titulo"+(id+1)+"</button></a><br>");
+			$("#preview_vista_botones").append("<button id=" + tt + " onclick='mostrar("+bb+")' >Titulo"+(id+1)+"</button><br>");
             $("#preview_vista_texto").append("<div id=" + bb + "></div>");
             asd = new nicEditor({ fullPanel: true }).panelInstance(b);
             id++;
