1e94523ebac28558548dfa1dd6d4ddd84c1860c8
Alan <alan.collante@gmail.com>
2013-01-22 12:59:12 -0300

    separacion de contacto y redes sociales

--- RedMovil.RedMovil.UserInterface/Scripts/mostrar.js
+++ RedMovil.RedMovil.UserInterface/Scripts/mostrar.js
@@ -11,53 +11,20 @@ function camposValidos(email, telefono) {
 
 function mostrarTodo() {
 
-
-        if (document.getElementById('mail').value == "" && document.getElementById('tel').value != "" && document.getElementById('address').value != ""){
-		
-	    $("#preview_vista_contacto").html('<center></br><center><input type="button" value="Llamar" onclick="parent.location=\'tel:+56'
-            + document.getElementById('tel').value
-            + '\'"><center></br><center><input type="button" value="Ubicacion" onclick="mostrarMapa()"></center>');
-	}
 	
-	if (document.getElementById('mail').value != "" && document.getElementById('tel').value == "" && document.getElementById('address').value != ""){
+	if (document.getElementById('tel').value != ""){
 		
-	    if (camposValidos(document.getElementById('mail').value, document.getElementById('tel').value)) {
-	    
-	    $("#preview_vista_contacto").html(
-            '<center><input type="button" value="Enviar email" onclick="parent.location=\'mailto:'
-            + document.getElementById('mail').value
-            + '\'"><center></br><center><input type="button" value="Ubicacion" onclick="mostrarMapa()"></center>');
-	    }
-	    
-	}
-	
-	if (document.getElementById('mail').value != "" && document.getElementById('tel').value != "" && document.getElementById('address').value == ""){
-		
-	    if (camposValidos(document.getElementById('mail').value, document.getElementById('tel').value)) {
-	    
-	    $("#preview_vista_contacto").html(
-            '<center><input type="button" value="Enviar email" onclick="parent.location=\'mailto:'
-            + document.getElementById('mail').value
-            + '\'"><center></br><center><input type="button" value="Llamar" onclick="parent.location=\'tel:+56'
-            + document.getElementById('tel').value
-            + '\'">');
-	    }
-		
-	}
-	
-	if (document.getElementById('mail').value == "" && document.getElementById('tel').value != "" && document.getElementById('address').value == ""){
-		
-	    $("#preview_vista_contacto").html(
+	    $("#llamanos").html(
             '<center></br><center><input type="button" value="Llamar" onclick="parent.location=\'tel:+56'
             + document.getElementById('tel').value
             + '\'">');
 	    
 	}
 	
-	if (document.getElementById('mail').value != "" && document.getElementById('tel').value == "" && document.getElementById('address').value == ""){
+	if (document.getElementById('mail').value != ""){
 		
 	    if (camposValidos(document.getElementById('mail').value, document.getElementById('tel').value)) {	
-	    $("#preview_vista_contacto").html(
+	    $("#escribenos").html(
             '<center><input type="button" value="Enviar email" onclick="parent.location=\'mailto:'
             + document.getElementById('mail').value
             + '\'">');
@@ -65,25 +32,13 @@ function mostrarTodo() {
 	    
 	}
 	 
-	if (document.getElementById('mail').value == "" && document.getElementById('tel').value == "" && document.getElementById('address').value != ""){   
+	if (document.getElementById('address').value != ""){   
 	    
-	    $("#preview_vista_contacto").html(
+	    $("#ubicanos").html(
             '<center></br><center><input type="button" value="Ubicacion" onclick="mostrarMapa()"></center>');
 	    
 	}
 	    
-	if (document.getElementById('mail').value != "" && document.getElementById('tel').value != "" && document.getElementById('address').value != ""){    
-	    
-	    
-	    if (camposValidos(document.getElementById('mail').value, document.getElementById('tel').value)) {
-	    $("#preview_vista_contacto").html(
-            '<center><input type="button" value="Enviar email" onclick="parent.location=\'mailto:'
-            + document.getElementById('mail').value
-            + '\'"><center></br><center><input type="button" value="Llamar" onclick="parent.location=\'tel:+56'
-            + document.getElementById('tel').value
-            + '\'"><center></br><center><input type="button" value="Ubicacion" onclick="mostrarMapa()"></center>');
-	    }
-	}
         
     
 }
--- RedMovil.RedMovil.UserInterface/Scripts/rs.js
+++ RedMovil.RedMovil.UserInterface/Scripts/rs.js
@@ -1,6 +1,6 @@
 function mostrarRS(){
             
-            if (document.getElementById('face').value != "" && document.getElementById('twit').value == ""){
+            if (document.getElementById('face').value != ""){
                         
             $("#facebook").html(
             '<center><input type="button" value="Facebook" onclick="parent.location=\'http://www.facebook.com/'
@@ -8,21 +8,12 @@ function mostrarRS(){
             + '\'">');
             }
             
-            if (document.getElementById('face').value == "" && document.getElementById('twit').value != ""){
+            if (document.getElementById('twit').value != ""){
                         
             $("#twitter").html(
             '<center></br><center><input type="button" value="Twitter" onclick="parent.location=\'http://www.twitter.com/'
             + document.getElementById('twit').value
             + '\'">');
             }
-            
-            if (document.getElementById('face').value != "" && document.getElementById('twit').value != ""){
-                        
-            $("#preview_vista_rs").html(
-            '<center><input type="button" value="Facebook" onclick="parent.location=\'http://www.facebook.com/'
-            + document.getElementById('face').value
-            + '\'"><center></br><center><input type="button" value="Twitter" onclick="parent.location=\'http://www.twitter.com/'
-            + document.getElementById('twit').value
-            + '\'">');
-            }
+                       
 }
