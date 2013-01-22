cc687b1a4a2bc5b6ef05a5bf42097337d1563a60
EDWARD SALAS <edwardsala@gmail.com>
2013-01-22 12:42:21 -0300

    tratodo de darle estilos css a los botones
    Signed-off-by: EDWARD SALAS <edwardsala@gmail.com>

--- RedMovil.RedMovil.UserInterface/Scripts/rs.js
+++ RedMovil.RedMovil.UserInterface/Scripts/rs.js
@@ -2,7 +2,7 @@ function mostrarRS(){
             
             if (document.getElementById('face').value != "" && document.getElementById('twit').value == ""){
                         
-            $("#preview_vista_rs").html(
+            $("#facebook").html(
             '<center><input type="button" value="Facebook" onclick="parent.location=\'http://www.facebook.com/'
             + document.getElementById('face').value
             + '\'">');
@@ -10,7 +10,7 @@ function mostrarRS(){
             
             if (document.getElementById('face').value == "" && document.getElementById('twit').value != ""){
                         
-            $("#preview_vista_rs").html(
+            $("#twitter").html(
             '<center></br><center><input type="button" value="Twitter" onclick="parent.location=\'http://www.twitter.com/'
             + document.getElementById('twit').value
             + '\'">');
