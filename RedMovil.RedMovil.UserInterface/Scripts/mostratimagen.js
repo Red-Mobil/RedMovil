f19a6f1a0d88f6e272773d385e9177d70df35bd2
Jullius <redmovil.dev@gmail.com>
2013-01-23 12:54:07 -0300

    boton imagen

--- RedMovil.RedMovil.UserInterface/Scripts/mostratimagen.js
+++ RedMovil.RedMovil.UserInterface/Scripts/mostratimagen.js
@@ -1,13 +1,24 @@
 function mostrarimagen() 
 {
-	var texto = '<div class="imageRow"><div class="set">';	
-	texto = texto + '<div class="single first"><a href="../images/' + document.getElementById('f1').value + '"rel="lightbox[plants]" title="Texto 1."><img src="../images/' + document.getElementById('f1').value + '"width="100" alt="Plants: image 1 0f 4 thumb" /></a></div>';
-	
-	
-	
-	texto = texto + '<div class="single"><a href="../images/' + document.getElementById('f2').value + '"rel="lightbox[plants]" title="Texto 2."><img src="../images/' + document.getElementById('f2').value + '"width="100" alt="Plants: image 1 0f 4 thumb" /></a></div>';
-	texto = texto + '<div class="single last"><a href="../images/' + document.getElementById('f3').value + '"rel="lightbox[plants]" title="Texto 3."><img src="../images/' + document.getElementById('f3').value + '"width="100" alt="Plants: image 1 0f 4 thumb" /></a></div>';
+	var texto = '<center></br><center><input type="button" value="Galeria" onclick="mostrarimagenes()" id="botonimagen" \></br></br>';		
+
+	texto = texto + '<div class="imageRow"><div class="set">';	
+	texto = texto + '<div class="single first" id=i1><a href="../images/' + document.getElementById('f1').value + '"rel="lightbox[plants]" title="Texto 1."><img src="../images/' + document.getElementById('f1').value + '"width="100" alt="Plants: image 1 0f 4 thumb"/></a></div>';
+	texto = texto + '<div class="single" id=i2><a href="../images/' + document.getElementById('f2').value + '"rel="lightbox[plants]" title="Texto 2."><img src="../images/' + document.getElementById('f2').value + '"width="100" alt="Plants: image 1 0f 4 thumb" /></a></div>';
+	texto = texto + '<div class="single last" id=i3><a href="../images/' + document.getElementById('f3').value + '"rel="lightbox[plants]" title="Texto 3."><img src="../images/' + document.getElementById('f3').value + '"width="100" alt="Plants: image 1 0f 4 thumb" /></a></div>';
 	texto = texto +'</div></div>';
 						
 	$("#preview_vista_galeria").html(texto);
+	document.getElementById('i1').style.display = 'none';
+	document.getElementById('i2').style.display = 'none';
+	document.getElementById('i3').style.display = 'none';
+}
+
+function mostrarimagenes()
+{
+	document.getElementById('home').style.display = 'block';
+	document.getElementById('botonimagen').style.display = 'none';
+	document.getElementById('i1').style.display = 'block';
+	document.getElementById('i2').style.display = 'block';
+	document.getElementById('i3').style.display = 'block';
 }
