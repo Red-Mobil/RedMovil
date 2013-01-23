0b72429f58dbaf43c6647cdd4515315372602fd8
Taufik <redmovil.dev@gmail.com>
2013-01-23 16:52:15 -0300

    crear

--- RedMovil.RedMovil.Backend/crear.php
+++ RedMovil.RedMovil.Backend/crear.php
@@ -17,12 +17,18 @@ fwrite($fp, "	<script src="../ExtLibs/jquery-1.8.3.min.js" type="text/javascript
 fwrite($fp, "	<script src="../ExtLibs/nicEdit.js" type="text/javascript"></script>");
 fwrite($fp, "	<script type="text/javascript" src='https://maps.googleapis.com/maps/api/js?key=AIzaSyDJh5o52Jocg2sFYFF6RI3O_sv9axSmbVw&sensor=false'></script>");
 fwrite($fp, "	<script type="text/javascript">        var id = 0; </script>");
-fwrite($fp, "
-fwrite($fp, "
-fwrite($fp, "
-fwrite($fp, "
-fwrite($fp, "
-fwrite($fp, "
-fwrite($fp, "
+fwrite($fp, "	<script type="text/javascript">        var max = 3;</script>");
+fwrite($fp, "	<script type="text/javascript">        var imagen = new Array();</script>");
+fwrite($fp, "	<script type="text/javascript">        var c_ima = 0; </script>");
+fwrite($fp, "	<script src="../Scripts/fotos.js" type="text/javascript"></script>");
+fwrite($fp, "	<script src="../Scripts/mapa.js" type="text/javascript"></script>");
+fwrite($fp, "	<script src="../Scripts/mostrar.js" type="text/javascript"></script>");
+fwrite($fp, "	<script src="../Scripts/valfono.js" type="text/javascript"></script>");
+fwrite($fp, "	<script src="../Scripts/valmail.js" type="text/javascript"></script>");
+fwrite($fp, "	<script src="../Scripts/ver.js" type="text/javascript"></script>");
+fwrite($fp, "	<script src="../Scripts/rs.js" type="text/javascript"></script>");
+fwrite($fp, "	<script src="../Scripts/append.js" type="text/javascript"></script>");
+fwrite($fp, "	<link href="../Css/estilo.css" rel="stylesheet" type="text/css" />");
+fwrite($fp, "	<link href="../Css/estilomovil.css" rel="stylesheet" type="text/css">");
 
 ?>
