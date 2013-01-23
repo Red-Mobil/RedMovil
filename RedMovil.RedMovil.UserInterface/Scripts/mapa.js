7576991e41fa5eeb7c7a6f64d82361cbfb26aeba
Alan <alan.collante@gmail.com>
2013-01-23 17:14:28 -0300

    asd

--- RedMovil.RedMovil.UserInterface/Scripts/mapa.js
+++ RedMovil.RedMovil.UserInterface/Scripts/mapa.js
@@ -4,7 +4,7 @@ function inicializarGoogleMaps() {
     geocoder = new google.maps.Geocoder();
     var latlng = new google.maps.LatLng(-33.444314, -70.653524);
     var mapOptions = {
-        zoom: 18,
+        zoom: 17,
         center: latlng,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     }
