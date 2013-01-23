b7a4dedeaced622321aaa4d244d5bab10330083f
Jullius <redmovil.dev@gmail.com>
2013-01-23 11:20:48 -0300

    scroll funcionando

--- RedMovil.RedMovil.UserInterface/Scripts/mostratimagen.js
+++ RedMovil.RedMovil.UserInterface/Scripts/mostratimagen.js
@@ -1,19 +1,13 @@
 function mostrarimagen() 
 {
-	c_ima = 0;
-	var texto = '<center></br><center><input type="button" value="'+ document.getElementById('ng').value +'"\></br></br>';	
+	var texto = '<div class="imageRow"><div class="set">';	
+	texto = texto + '<div class="single first"><a href="../images/' + document.getElementById('f1').value + '"rel="lightbox[plants]" title="Texto 1."><img src="../images/' + document.getElementById('f1').value + '"width="100" alt="Plants: image 1 0f 4 thumb" /></a></div>';
 	
-	if (document.getElementById('f1').value != "")
-	{	
-		texto = texto + '<img src="../images/'+ document.getElementById('f1').value +'" width="100"></br>';
-	}	
-	if (document.getElementById('f2').value != "")
-	{	
-		texto = texto + '<img src="../images/'+ document.getElementById('f2').value +'" width="100"></br>';
-	}	
-	if (document.getElementById('f3').value != "")
-	{	
-		texto = texto + '<img src="../images/'+ document.getElementById('f3').value +'" width="100"></br></br>';
-	}			
+	
+	
+	texto = texto + '<div class="single"><a href="../images/' + document.getElementById('f2').value + '"rel="lightbox[plants]" title="Texto 2."><img src="../images/' + document.getElementById('f2').value + '"width="100" alt="Plants: image 1 0f 4 thumb" /></a></div>';
+	texto = texto + '<div class="single last"><a href="../images/' + document.getElementById('f3').value + '"rel="lightbox[plants]" title="Texto 3."><img src="../images/' + document.getElementById('f3').value + '"width="100" alt="Plants: image 1 0f 4 thumb" /></a></div>';
+	texto = texto +'</div></div>';
+						
 	$("#preview_vista_galeria").html(texto);
 }
