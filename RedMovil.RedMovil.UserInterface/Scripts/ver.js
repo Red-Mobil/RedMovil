83df232846449c77998626069389e96e1c192866
Taufik <redmovil.dev@gmail.com>
2013-01-25 20:19:31 -0300

    a

--- RedMovil.RedMovil.UserInterface/Scripts/ver.js
+++ RedMovil.RedMovil.UserInterface/Scripts/ver.js
@@ -3,17 +3,17 @@ function ver() {
         document.getElementById("tt0").innerHTML = document.getElementById("t0").value;
         var z = nicEditors.findEditor("b0");
         var y = z.getContent();
-        document.getElementById("bb0").innerHTML = document.getElementById("t0").value + "<br>" + y;
+        document.getElementById("bb0").innerHTML = y;
     }
     if (max == 1) {
         document.getElementById("tt0").innerHTML = document.getElementById("t0").value;
         document.getElementById("tt1").innerHTML = document.getElementById("t1").value;
         var z = nicEditors.findEditor("b0");
         var y = z.getContent();
-        document.getElementById("bb0").innerHTML = document.getElementById("t0").value + "<br>" + y;
+        document.getElementById("bb0").innerHTML = y;
         var p = nicEditors.findEditor("b1");
         var o = p.getContent();
-        document.getElementById("bb1").innerHTML = document.getElementById("t1").value + "<br>" + o;
+        document.getElementById("bb1").innerHTML = o;
     }
     if (max == 0) {
         document.getElementById("tt0").innerHTML = document.getElementById("t0").value;
@@ -21,12 +21,12 @@ function ver() {
         document.getElementById("tt2").innerHTML = document.getElementById("t2").value;
         var z = nicEditors.findEditor("b0");
         var y = z.getContent();
-        document.getElementById("bb0").innerHTML = document.getElementById("t0").value + "<br>" + y;
+        document.getElementById("bb0").innerHTML = y;
         var p = nicEditors.findEditor("b1");
         var o = p.getContent();
-        document.getElementById("bb1").innerHTML = document.getElementById("t1").value + "<br>" + o;
+        document.getElementById("bb1").innerHTML = o;
         var i = nicEditors.findEditor("b2");
         var u = i.getContent();
-        document.getElementById("bb2").innerHTML = document.getElementById("t2").value + "<br>" + u;
+        document.getElementById("bb2").innerHTML = u;
     }
 }
