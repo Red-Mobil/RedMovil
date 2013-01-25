a493cbd7103664bbb51bd9a42da2d8aa4b93fd7d
EDWARD SALAS <edwardsala@gmail.com>
2013-01-25 19:55:16 -0300

    creacion de clases simplificacion del css
    Signed-off-by: EDWARD SALAS <edwardsala@gmail.com>

--- RedMovil.RedMovil.UserInterface/Scripts/append.js
+++ RedMovil.RedMovil.UserInterface/Scripts/append.js
@@ -332,8 +332,8 @@ $(document).ready(function () {
             bb = "bb".concat(id);
             $("#texto").append("<textarea id=" + t + " style='width:320px;' >Titulo"+(id+1)+"</textarea><textarea id=" + b + " style='width:320px;' ></textarea><br>");
             //$("#preview_vista_botones").append("<a href=#" + bb + "><button id=" + tt + " >Titulo"+(id+1)+"</button></a><br>");
-			$("#preview_vista_botones").append("<button id=" + tt + " onclick='mostrar ("+bb+")' >Titulo"+(id+1)+"</button><br>");
-            $("#preview_vista_texto").append("<div id=" + bb + "></div>");
+			$("#preview_vista_botones").append("<button class='tt' id=" + tt + " onclick='mostrar ("+bb+")' >Titulo"+(id+1)+" </button><br>");
+            $("#preview_vista_texto").append("<div class='bb'id=" + bb + "></div>");
             asd = new nicEditor({ fullPanel: true }).panelInstance(b);
             id++;
             max -= 1;
