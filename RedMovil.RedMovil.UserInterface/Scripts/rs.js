b1a46078f686b477747e04ac22c99d4f6936f821
EDWARD SALAS <edwardsala@gmail.com>
2013-01-24 21:33:54 -0300

    cambios en el rs js par diseño de botones

--- RedMovil.RedMovil.UserInterface/Scripts/rs.js
+++ RedMovil.RedMovil.UserInterface/Scripts/rs.js
@@ -3,7 +3,7 @@ function mostrarRS(){
             if (document.getElementById('face').value != ""){
                         
             $("#facebook").html(
-            '<center><input type="button" id="face" class="contacto"value="" onclick="parent.location=\'http://www.facebook.com/'
+            '<center><input type="button" id="faceb" class="contacto"value="" onclick="parent.location=\'http://www.facebook.com/'
             + document.getElementById('face').value
             + '\'">');
             }
@@ -11,7 +11,7 @@ function mostrarRS(){
             if (document.getElementById('twit').value != ""){
                         
             $("#twitter").html(
-            '<center></br><center><input id="twit" class="contacto" value="" type="button"  onclick="parent.location=\'http://www.twitter.com/'
+            '<center></br><center><input id="twitt" class="contacto" value="" type="button"  onclick="parent.location=\'http://www.twitter.com/'
             + document.getElementById('twit').value
             + '\'">');
             }
