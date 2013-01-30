48b48405a8ac97aed092e52e378061db33800571
Taufik <redmovil.dev@gmail.com>
2013-01-30 11:21:57 -0300

    asd

--- RedMovil.RedMovil.Backend/copy.php
+++ RedMovil.RedMovil.Backend/copy.php
@@ -3,9 +3,9 @@
 
 
 $file=$_GET["file"];
-$file = "../RedMovil.RedMovil.UserInterface/Plantillas/".$file;
+$aux = "../RedMovil.RedMovil.UserInterface/Plantillas/".$file;
 $newfile = "../RedMovil.RedMovil.UserInterface/Plantillas/new".$file;
-if (!copy($file, $newfile)) {
+if (!copy($aux, $newfile)) {
     echo "-1";
 }
 else{
