21eb52136080fd7f8a276f0ae9d65187226d00a5
Alan <alan.collante@gmail.com>
2013-01-30 10:56:38 -0300

    adasd

--- RedMovil.RedMovil.UserInterface/Scripts/cambiarplantilla.js
+++ RedMovil.RedMovil.UserInterface/Scripts/cambiarplantilla.js
@@ -1,5 +1,10 @@
+var nombrecss;
+
 function cambiarArchivoCss(archivo) {
     
     document.getElementById('cssArchivo').href=archivo;
     
+    var algo = archivo.split("/");
+    nombrecss = algo[2];
+    copy(nombrecss);
 }
