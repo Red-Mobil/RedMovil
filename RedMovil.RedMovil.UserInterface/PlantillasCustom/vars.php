67cf6726e20fc408523621e5b7077611436be068
Alan <alan.collante@gmail.com>
2013-01-31 09:50:53 -0300

    asd

---
+++ RedMovil.RedMovil.UserInterface/PlantillasCustom/vars.php
@@ -0,0 +1,11 @@
+<?php
+	$imagenfondo = "url(../imagenes/arab.png)";
+	$colorfondo = "#000";
+	$colorbotonesini = "#ffe8ff";
+	$colorbotonesfin = "#d8c3e8";
+	$colortitulo = "#906";
+	$letrasbotones = "#333";
+	$letrasparrafo = "#906";
+	$fondoparrafo = "#f6edfa";
+	$bordebotones = "#000";
+?>
