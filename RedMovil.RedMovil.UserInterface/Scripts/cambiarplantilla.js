7ba28008aa90bbba3bd6c27526c152883b9a33fe
Taufik <redmovil.dev@gmail.com>
2013-02-01 12:36:24 -0300

    plantillas

--- RedMovil.RedMovil.UserInterface/Scripts/cambiarplantilla.js
+++ RedMovil.RedMovil.UserInterface/Scripts/cambiarplantilla.js
@@ -1,9 +1,42 @@
 var nombrecss;
 
 function cambiarArchivoCss(archivo) {
+    var algo = archivo.split("/");
+    nombrecss = algo[2];
+	if (algo[2] == "lana.css")
+	{
+		$("#c0").val("../imagenes/arab.png");
+		$("#c1").val("FFFFFF");
+		$("#c2").val("906");
+		$("#c3").val("FFE8FF");
+		$("#c4").val("333");
+		$("#c5").val("F6EDFA");
+		$("#c6").val("906");
+		$("#c7").val("FFFFFF");
+	}
+	else if (algo[2] == "diente.css")
+	{
+		$("#c0").val("../imagenes/furley_bg.png");
+		$("#c1").val("FFFFFF");
+		$("#c2").val("999");
+		$("#c3").val("FFEFFF");
+		$("#c4").val("000");
+		$("#c5").val("BDD1E3");
+		$("#c6").val("FFFFFF");
+		$("#c7").val("FFFFFF");
+	}
+	else if (algo[2] == "black.css")
+	{
+		$("#c0").val("../imagenes/iphone.png");
+		$("#c1").val("000");
+		$("#c2").val("FFF");
+		$("#c3").val("666");
+		$("#c4").val("FFF");
+		$("#c5").val("555");
+		$("#c6").val("fff");
+		$("#c7").val("777");
+	}
     mostrarbotones();  
     document.getElementById('cssArchivo').href=archivo;
     
-    var algo = archivo.split("/");
-    nombrecss = algo[2];   
 }
