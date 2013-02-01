5fbe7f01e3817df555a1ce48a0ff5ead0952b7f3
Taufik <redmovil.dev@gmail.com>
2013-02-01 19:28:42 -0300

    asd

--- RedMovil.RedMovil.Backend/generadorcss.php
+++ RedMovil.RedMovil.Backend/generadorcss.php
@@ -483,10 +483,10 @@
 	fwrite($fp, 'background-color: #79bbff;'.PHP_EOL);
 	fwrite($fp, 'background: '.$colorbotones.';'.PHP_EOL);
 	fwrite($fp, 'filter:progid:DXImageTransform.Microsoft.gradient(startColorstr="#666", endColorstr="#333");'.PHP_EOL);
-	fwrite($fp, 'border-top-color:	 '.$bordebotones.''.PHP_EOL);
-	fwrite($fp, 'border-left-color:	'.$bordebotones.''.PHP_EOL);
-	fwrite($fp, 'border-bottom-color: '.$bordebotones.''.PHP_EOL);
-	fwrite($fp, 'border-right-color:'.$bordebotones.''.PHP_EOL);
+	fwrite($fp, 'border-top-color:	 '.$bordebotones.';'.PHP_EOL);
+	fwrite($fp, 'border-left-color:	'.$bordebotones.';'.PHP_EOL);
+	fwrite($fp, 'border-bottom-color: '.$bordebotones.';'.PHP_EOL);
+	fwrite($fp, 'border-right-color:'.$bordebotones.';'.PHP_EOL);
 	fwrite($fp, 'color: '.$letrasbotones.';'.PHP_EOL);
 	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
 	
@@ -578,10 +578,10 @@
 	fwrite($fp, 'background-color: '.$bordebotones.''.PHP_EOL);
 	fwrite($fp, 'background: '.$colorbotones.';'.PHP_EOL);
 	fwrite($fp, 'filter:progid:DXImageTransform.Microsoft.gradient(startColorstr="#666", endColorstr="#333");'.PHP_EOL);
-	fwrite($fp, 'border-top-color:	 '.$bordebotones.''.PHP_EOL);
-	fwrite($fp, 'border-left-color:	 '.$bordebotones.''.PHP_EOL);
-	fwrite($fp, 'border-bottom-color: '.$bordebotones.''.PHP_EOL);
-	fwrite($fp, 'border-right-color:'.$bordebotones.''.PHP_EOL);
+	fwrite($fp, 'border-top-color:	 '.$bordebotones.';'.PHP_EOL);
+	fwrite($fp, 'border-left-color:	 '.$bordebotones.';'.PHP_EOL);
+	fwrite($fp, 'border-bottom-color: '.$bordebotones.';'.PHP_EOL);
+	fwrite($fp, 'border-right-color:'.$bordebotones.';'.PHP_EOL);
 	fwrite($fp, 'color: '.$letrasbotones.';'.PHP_EOL);
 	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
 	
@@ -843,6 +843,208 @@
 	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
 	
 	fwrite($fp, '#twitt:hover {'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '.imagerow{'.PHP_EOL);
+	fwrite($fp, 'margin-top: 2%;'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '.single,single first,single last {'.PHP_EOL);
+	fwrite($fp, 'width: 80%;'.PHP_EOL);
+	fwrite($fp, 'margin-top: 2%;'.PHP_EOL);
+	fwrite($fp, 'margin-left:10%;'.PHP_EOL);
+	fwrite($fp, '}');
+	
+	fclose($fp);
+	}
+	
+		//--Codigo glossy--//
+	
+	if($namecss == "glossy.css"){
+		
+	$fp = fopen("../RedMovil.RedMovil.UserInterface/PlantillasCustom/".$titulo.".css","w");
+	
+	fwrite($fp, "@charset 'utf-8';".PHP_EOL);
+	fwrite($fp, "body {margin:0px;".PHP_EOL."padding:opx;}".PHP_EOL);
+	
+	fwrite($fp, "#expandible {".PHP_EOL);
+	fwrite($fp, "min-height: 100%;".PHP_EOL);
+	fwrite($fp, "padding-bottom:25%;".PHP_EOL);
+	fwrite($fp, "width: 100%;".PHP_EOL);
+	fwrite($fp, "background-color:".$colorfondo.';'.PHP_EOL);
+	fwrite($fp, "background-image:".$imagenfondo.";".PHP_EOL);
+	fwrite($fp, "background-repeat: repeat;".PHP_EOL);
+	fwrite($fp, "background-attachment: fixed;".PHP_EOL);
+	fwrite($fp, "float: left;".PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, ".title{".PHP_EOL);
+	fwrite($fp, "width: 80%;".PHP_EOL);
+	fwrite($fp, "margin-left: 9%;".PHP_EOL);
+	fwrite($fp, "margin-top:3%;".PHP_EOL);
+	fwrite($fp, "padding-left:2%;".PHP_EOL);
+	fwrite($fp, "font-family: 'Trebuchet MS', Arial, Helvetica, sans-serif;".PHP_EOL);
+	fwrite($fp, "font-size: 150%;".PHP_EOL);
+	fwrite($fp, "font-weight: bolder;".PHP_EOL);
+	fwrite($fp, "text-transform: uppercase;".PHP_EOL);
+	fwrite($fp, "color:".$colortitulo.";".PHP_EOL);
+	fwrite($fp, "text-align: left;".PHP_EOL);
+	fwrite($fp, "float: left;".PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, ".tt{".PHP_EOL);
+	fwrite($fp, "font-weight: bolder;".PHP_EOL);
+	fwrite($fp, "text-transform: uppercase;".PHP_EOL);
+	fwrite($fp, 'margin-top: 4%;'.PHP_EOL);
+	fwrite($fp, 'float: left;'.PHP_EOL);
+	fwrite($fp, 'width: 105px;'.PHP_EOL);
+	fwrite($fp, 'height: 105px;'.PHP_EOL);
+	fwrite($fp, 'margin-left: 9%;'.PHP_EOL);
+	fwrite($fp, 'border-radius: 8px;'.PHP_EOL);
+	fwrite($fp, 'font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;'.PHP_EOL);
+	fwrite($fp, 'font-size:100%;'.PHP_EOL);
+	fwrite($fp, '-moz-border-radius: 13px;'.PHP_EOL);
+	fwrite($fp, '-webkit-border-radius: 13px;'.PHP_EOL);
+	fwrite($fp, 'border-radius: 13px;'.PHP_EOL);
+	fwrite($fp, 'border: 4px #'.$bordebotones.' solid;'.PHP_EOL);
+	fwrite($fp, 'background: #'.$colorbotones.' ;'.PHP_EOL);
+	fwrite($fp, 'border-top-color:	 #'.$bordebotones.' ;'.PHP_EOL);
+	fwrite($fp, 'border-left-color:	#'.$bordebotones.' ;'.PHP_EOL);
+	fwrite($fp, 'border-bottom-color: #'.$bordebotones.' ;'.PHP_EOL);
+	fwrite($fp, 'border-right-color:#'.$bordebotones.' ;'.PHP_EOL);
+	fwrite($fp, 'color: #'.$letrasbotones.';'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '.tt:hover{'.PHP_EOL);
+	fwrite($fp, 'background-color:#0CF;'.PHP_EOL);
+	fwrite($fp, 'text-shadow: 0px 0px 3px #FFF;'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '#home {'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '#home:hover{'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '.bb{'.PHP_EOL);
+	fwrite($fp, 'float: left;'.PHP_EOL);
+	fwrite($fp, 'width: 72%;'.PHP_EOL);
+	fwrite($fp, 'padding:4%;'.PHP_EOL);
+	fwrite($fp, 'min-height: 40%;'.PHP_EOL);
+	fwrite($fp, 'background-color: '.$fondoparrafo.';'.PHP_EOL);
+	fwrite($fp, 'margin-left: 9%;'.PHP_EOL);
+	fwrite($fp, 'border-radius: 7px;'.PHP_EOL);
+	fwrite($fp, 'color:	'.$letrasparrafo.';'.PHP_EOL);
+	fwrite($fp, 'margin-top:2%;'.PHP_EOL);
+	fwrite($fp, 'font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;'.PHP_EOL);
+	fwrite($fp, 'font-size: 100%;'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '#bb0 {'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '#bb1 {'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '#bb2 {'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '#mapa {'.PHP_EOL);
+	fwrite($fp, 'height: 100%;'.PHP_EOL);
+	fwrite($fp, 'width: 100%;'.PHP_EOL);
+	fwrite($fp, 'margin-top: 10px;'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '#tt0 {'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '#tt0:hover{'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '#tt1 {'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '#tt1:hover{'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '#tt2 {'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '#tt2:hover{'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+		
+	fwrite($fp, '#botonimagen{'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '#botonimagen:hover{'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '.contacto {'.PHP_EOL);
+	fwrite($fp, 'font-weight: bolder;'.PHP_EOL);
+	fwrite($fp, "text-transform: uppercase;".PHP_EOL);
+	fwrite($fp, 'margin-top: 4%;'.PHP_EOL);
+	fwrite($fp, 'float: left;'.PHP_EOL);
+	fwrite($fp, 'width: 105px;'.PHP_EOL);
+	fwrite($fp, 'height: 105px;'.PHP_EOL);
+	fwrite($fp, 'margin-left: 9%;'.PHP_EOL);
+	fwrite($fp, 'font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;'.PHP_EOL);
+	fwrite($fp, 'font-size:100%;'.PHP_EOL);
+	fwrite($fp, '-moz-border-radius: 13px;'.PHP_EOL);
+	fwrite($fp, '-webkit-border-radius: 13px;'.PHP_EOL);
+	fwrite($fp, 'border-radius: 13px;'.PHP_EOL);
+	fwrite($fp, 'border: 4px solid;'.PHP_EOL);
+	fwrite($fp, 'background-color: '.$colorbotones.';'.PHP_EOL);
+	fwrite($fp, 'border-top-color:	 '.$bordebotones.';'.PHP_EOL);
+	fwrite($fp, 'border-left-color:	 '.$bordebotones.';'.PHP_EOL);
+	fwrite($fp, 'border-bottom-color: '.$bordebotones.';'.PHP_EOL);
+	fwrite($fp, 'border-right-color:'.$bordebotones.';'.PHP_EOL);
+	fwrite($fp, 'color: '.$letrasbotones.';'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '.contacto:hover {'.PHP_EOL);
+	fwrite($fp, 'background-color:#0C6;'.PHP_EOL);
+	fwrite($fp, 'text-shadow: 0px 0px 3px #FFF;'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '#llamar {'.PHP_EOL);
+	
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '#llamar:hover {'.PHP_EOL);
+	fwrite($fp, 'background-color: #0F3;'.PHP_EOL);
+	fwrite($fp, 'border-color:#0f3;'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '#escribir {'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '#escribir:hover {'.PHP_EOL);
+	fwrite($fp, 'background-color: #F00;'.PHP_EOL);
+	fwrite($fp, '#border-color:#F00;'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '#ubicar {'.PHP_EOL);
+	fwrite($fp, 'background-color: #FC30;'.PHP_EOL);
+	fwrite($fp, '#border-color:#C30;'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '#ubicar:hover {'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '#faceb {'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '#faceb:hover {'.PHP_EOL);
+	fwrite($fp, 'background-color: #069;'.PHP_EOL);
+	fwrite($fp, '#border-color:#069;'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '#twitt {'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '#twitt:hover {'.PHP_EOL);
+	fwrite($fp, 'background-color: #0CF;'.PHP_EOL);
+	fwrite($fp, '#border-color:#0CF;'.PHP_EOL);
 	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
 	
 	fwrite($fp, '.imagerow{'.PHP_EOL);
