19fb0e875f61ebc70e069b26b507c1fc74ce2458
Alan <alan.collante@gmail.com>
2013-02-01 19:07:25 -0300

    asd

--- RedMovil.RedMovil.Backend/generadorcss.php
+++ RedMovil.RedMovil.Backend/generadorcss.php
@@ -637,4 +637,224 @@
 	fclose($fp);
 	}
 	
+	//--Codigo Estilomovil--//
+	
+	if($namecss == "estilomovil.css"){
+		
+	$fp = fopen("../RedMovil.RedMovil.UserInterface/PlantillasCustom/".$titulo.".css","w");
+	
+	fwrite($fp, "@charset 'utf-8';".PHP_EOL);
+	fwrite($fp, "body {margin:0px;".PHP_EOL."padding:opx;}".PHP_EOL);
+	
+	fwrite($fp, "#expandible {".PHP_EOL);
+	fwrite($fp, "min-height: 100%;".PHP_EOL);
+	fwrite($fp, "padding-bottom:25%;".PHP_EOL);
+	fwrite($fp, "width: 100%;".PHP_EOL);
+	fwrite($fp, "background-color:".$colorfondo.';'.PHP_EOL);
+	fwrite($fp, "background-image:".$imagenfondo.";".PHP_EOL);
+	fwrite($fp, "background-repeat: repeat;".PHP_EOL);
+	fwrite($fp, "background-attachment: fixed;".PHP_EOL);
+	fwrite($fp, "float: left;".PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '#home:before {'.PHP_EOL);
+	fwrite($fp, "content:url(../imagenes/home.svg);".PHP_EOL);
+	fwrite($fp, "float:left;".PHP_EOL);
+	fwrite($fp, "width:20%;".PHP_EOL);
+	fwrite($fp, "height:70%;".PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, ".title{".PHP_EOL);
+	fwrite($fp, "width: 80%;".PHP_EOL);
+	fwrite($fp, "margin-left: 11%;".PHP_EOL);
+	fwrite($fp, "padding-left:3%;".PHP_EOL);
+	fwrite($fp, "font-family: 'Trebuchet MS', Arial, Helvetica, sans-serif;".PHP_EOL);
+	fwrite($fp, "font-size: 150%;".PHP_EOL);
+	fwrite($fp, "font-weight: bolder;".PHP_EOL);
+	fwrite($fp, "text-transform: uppercase;".PHP_EOL);
+	fwrite($fp, "color:".$colortitulo.";".PHP_EOL);
+	fwrite($fp, "text-align: left;".PHP_EOL);
+	fwrite($fp, "float: left;".PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, ".tt{".PHP_EOL);
+	fwrite($fp, "width: 80%;".PHP_EOL);
+	fwrite($fp, "height: 40px;".PHP_EOL);
+	fwrite($fp, 'font-family: "Trebuchet MS", 	Arial, Helvetica, sans-serif;'.PHP_EOL);
+	fwrite($fp, 'font-weight: bolder;'.PHP_EOL);
+	fwrite($fp, 'text-transform: uppercase;'.PHP_EOL);
+	fwrite($fp, 'font-size: 130%;'.PHP_EOL);
+	fwrite($fp, 'float: left;'.PHP_EOL);
+	fwrite($fp, 'margin-left: 11%;'.PHP_EOL);
+	fwrite($fp, 'margin-top: 3px;'.PHP_EOL);
+	fwrite($fp, '-moz-border-radius: 13px;'.PHP_EOL);
+	fwrite($fp, '-webkit-border-radius: 13px;'.PHP_EOL);
+	fwrite($fp, 'border-radius: 7px;'.PHP_EOL);
+	fwrite($fp, 'border: 0px solid '.$bordebotones.';'.PHP_EOL);
+	fwrite($fp, 'text-decoration: none;'.PHP_EOL);
+	fwrite($fp, 'filter:progid:DXImageTransform.Microsoft.gradient(startColorstr="#79bbff", endColorstr="#378de5");'.PHP_EOL);
+	fwrite($fp, 'background-color: #79bbff;'.PHP_EOL);
+	fwrite($fp, 'background: '.$colorbotones.';'.PHP_EOL);
+	fwrite($fp, 'text-shadow: 1px 0px 22px #528ecc;'.PHP_EOL);
+	fwrite($fp, '-webkit-box-shadow: inset 0px 0px 0px -50px #bbdaf7;'.PHP_EOL);
+	fwrite($fp, '-moz-box-shadow: inset 0px 0px 0px -50px #bbdaf7;'.PHP_EOL);
+	fwrite($fp, 'box-shadow: inset 0px 0px 0px -50px #bbdaf7;'.PHP_EOL);
+	fwrite($fp, 'color: '.$letrasbotones.';'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '.tt:hover{'.PHP_EOL);
+	fwrite($fp, 'background:-webkit-gradient( linear, left top, left bottom, color-stop(46%, #378de5), color-stop(59%, #79bbff) );'.PHP_EOL);
+	fwrite($fp, 'background:-moz-linear-gradient( center top, #378de5 46%, #79bbff 59% );'.PHP_EOL);
+	fwrite($fp, 'background:-ms-linear-gradient( top, #378de5 46%, #79bbff 59% );'.PHP_EOL);
+	fwrite($fp, 'filter:progid:DXImageTransform.Microsoft.gradient(startColorstr="#378de5", endColorstr="#79bbff");'.PHP_EOL);
+	fwrite($fp, 'background-color:#378de5;'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '#home:hover{'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '.bb{'.PHP_EOL);
+	fwrite($fp, 'float: left;'.PHP_EOL);
+	fwrite($fp, 'width: 72%;'.PHP_EOL);
+	fwrite($fp, 'padding:4%;'.PHP_EOL);
+	fwrite($fp, 'min-height: 40%;'.PHP_EOL);
+	fwrite($fp, 'background-color: '.$fondoparrafo.';'.PHP_EOL);
+	fwrite($fp, 'margin-left: 11%;'.PHP_EOL);
+	fwrite($fp, 'border-radius: 7px;'.PHP_EOL);
+	fwrite($fp, 'color:	'.$letrasparrafo.';'.PHP_EOL);
+	fwrite($fp, 'margin-top:2%;'.PHP_EOL);
+	fwrite($fp, 'font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;'.PHP_EOL);
+	fwrite($fp, 'font-size: 100%;'.PHP_EOL);
+	fwrite($fp, 'filter: progid:DXImageTransform.Microsoft.Alpha(Opacity=85);'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '#bb0 {'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '#bb1 {'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '#bb2 {'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '#mapa {'.PHP_EOL);
+	fwrite($fp, 'height: 100%;'.PHP_EOL);
+	fwrite($fp, 'width: 100%;'.PHP_EOL);
+	fwrite($fp, 'margin-top: 10px;'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '#tt0:before {'.PHP_EOL);
+	fwrite($fp, "content:url(../imagenes/tt0.svg);".PHP_EOL);
+	fwrite($fp, "float:left;".PHP_EOL);
+	fwrite($fp, "width:20%;".PHP_EOL);
+	fwrite($fp, "height:70%;".PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '#tt0:hover{'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '#tt1:before {'.PHP_EOL);
+	fwrite($fp, "content:url(../imagenes/tt1.svg);".PHP_EOL);
+	fwrite($fp, "float:left;".PHP_EOL);
+	fwrite($fp, "width:20%;".PHP_EOL);
+	fwrite($fp, "height:70%;".PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '#tt1:hover{'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '#tt2:before {'.PHP_EOL);
+	fwrite($fp, "content:url(../imagenes/tt2.svg);".PHP_EOL);
+	fwrite($fp, "float:left;".PHP_EOL);
+	fwrite($fp, "width:20%;".PHP_EOL);
+	fwrite($fp, "height:70%;".PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '#tt2:hover{'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+		
+	fwrite($fp, '#botonimagen{'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '#botonimagen:hover{'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '.contacto {'.PHP_EOL);
+	fwrite($fp, 'font-weight: bolder;'.PHP_EOL);
+	fwrite($fp, 'text-transform: uppercase;'.PHP_EOL);
+	fwrite($fp, 'font-size: large;'.PHP_EOL);
+	fwrite($fp, 'margin-top: 3px;'.PHP_EOL);
+	fwrite($fp, 'float: left;'.PHP_EOL);
+	fwrite($fp, 'width: 80%;'.PHP_EOL);
+	fwrite($fp, 'height: 40px;'.PHP_EOL);
+	fwrite($fp, 'margin-left: 11%;'.PHP_EOL);
+	fwrite($fp, '-webkit-border-radius: 8px;'.PHP_EOL);
+	fwrite($fp, '-moz-border-radius: 8px;'.PHP_EOL);
+	fwrite($fp, 'border-radius: 8px;'.PHP_EOL);
+	fwrite($fp, 'font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;'.PHP_EOL);
+	fwrite($fp, 'font-size: 130%;'.PHP_EOL);
+	fwrite($fp, 'color: '.$letrasbotones.';'.PHP_EOL);
+	fwrite($fp, '-moz-border-radius: 13px;'.PHP_EOL);
+	fwrite($fp, '-webkit-border-radius: 13px;'.PHP_EOL);
+	fwrite($fp, 'border-radius: 7px;'.PHP_EOL);
+	fwrite($fp, 'background: '.$colorbotones.';'.PHP_EOL);
+	fwrite($fp, 'filter:progid:DXImageTransform.Microsoft.gradient(startColorstr="#79bbff", endColorstr="#378de5");'.PHP_EOL);
+	fwrite($fp, 'border: 0px solid '.$bordebotones.';'.PHP_EOL);
+	fwrite($fp, 'text-decoration: none;'.PHP_EOL);
+	fwrite($fp, 'text-shadow: 1px 0px 22px #528ecc;'.PHP_EOL);
+	fwrite($fp, '-webkit-box-shadow: inset 0px 0px 0px -50px #bbdaf7;'.PHP_EOL);
+	fwrite($fp, '-moz-box-shadow: inset 0px 0px 0px -50px #bbdaf7;'.PHP_EOL);
+	fwrite($fp, 'box-shadow: inset 0px 0px 0px -50px #bbdaf7;'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '.contacto:hover {'.PHP_EOL);
+	fwrite($fp, 'background:-webkit-gradient( linear, left top, left bottom, color-stop(46%, #378de5), color-stop(59%, #79bbff) );'.PHP_EOL);
+	fwrite($fp, 'background:-moz-linear-gradient( center top, #378de5 46%, #79bbff 59% );'.PHP_EOL);
+	fwrite($fp, 'background:-ms-linear-gradient( top, #378de5 46%, #79bbff 59% );'.PHP_EOL);
+	fwrite($fp, 'filter:progid:DXImageTransform.Microsoft.gradient(startColorstr="#378de5", endColorstr="#79bbff");'.PHP_EOL);
+	fwrite($fp, 'background-color:#378de5;'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '#llamar {'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '#llamar:hover {'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '#escribir {'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '#escribir:hover {'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '#ubicar {'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '#ubicar:hover {'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '#faceb {'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '#faceb:hover {'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '#twitt {'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '#twitt:hover {'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '.imagerow{'.PHP_EOL);
+	fwrite($fp, 'margin-top: 2%;'.PHP_EOL);
+	fwrite($fp, '}'.PHP_EOL.PHP_EOL);
+	
+	fwrite($fp, '.single,single first,single last {'.PHP_EOL);
+	fwrite($fp, 'width: 80%;'.PHP_EOL);
+	fwrite($fp, 'margin-top: 2%;'.PHP_EOL);
+	fwrite($fp, 'margin-left:10%;'.PHP_EOL);
+	fwrite($fp, '}');
+	
+	fclose($fp);
+	}
 ?>
