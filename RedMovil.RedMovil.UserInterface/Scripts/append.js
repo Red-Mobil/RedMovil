eef7f02aa2631b4e7d19279b8a4a239a5e416e50
Taufik <redmovil.dev@gmail.com>
2013-02-04 15:33:09 -0300

    boton seleccionar imagen

--- RedMovil.RedMovil.UserInterface/Scripts/append.js
+++ RedMovil.RedMovil.UserInterface/Scripts/append.js
@@ -463,7 +463,7 @@ $(document).ready(function () {
             tt = "tt".concat(id);
             bb = "bb".concat(id);
 			title = "title".concat(id);
-			$("#texto").append("<div><a href='javascript:showPopup(\""+aux[id]+"\")'>Seleccione Imagen</a><br>");
+			$("#texto").append("<div><a href='javascript:showPopup(\""+aux[id]+"\")'><input type='button' value='Seleccione Imagen'/</a><br>");
 			$("#texto").append("<input type='hidden' id ="+h+" value='vacio'>");
             $("#texto").append("<textarea id=" + t + " style='width:320px;' >Titulo"+(id+1)+"</textarea><textarea id=" + b + " style='width:320px;' ></textarea><br>");
             //$("#preview_vista_botones").append("<a href=#" + bb + "><button id=" + tt + " >Titulo"+(id+1)+"</button></a><br>");
