438f181858aabd339d82490692b9097cdf787a2a
Jullius <redmovil.dev@gmail.com>
2013-02-06 13:42:47 -0300

    123

--- RedMovil.RedMovil.Backend/connection.php
+++
@@ -1,5 +0,0 @@
-<?php
-$connection_string="host=localhost port=5432 user=postgres password=matias dbname=redmovil";
-//$connection_string="host=localhost port=5432 user=redmovil password=z15as12xx dbname=redmovil";
-$conn = pg_connect($connection_string) or die("Falló la conexión.");
-?>
