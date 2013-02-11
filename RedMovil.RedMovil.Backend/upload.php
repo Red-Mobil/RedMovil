8533226fff7a328a604551c63b47360e98af3cc7
Jullius <redmovil.dev@gmail.com>
2013-02-11 15:56:02 -0300

    redemiensionado

--- RedMovil.RedMovil.Backend/upload.php
+++ RedMovil.RedMovil.Backend/upload.php
@@ -1,10 +1,18 @@
 <?php		
+require("class.image-resize.php"); //incluir la clase en el archivo
+
 		$directorio = '../RedMovil.RedMovil.UserInterface/images/';
 		$tmpName  = $_FILES['userfile']['tmp_name'];
 		echo $tmpName;
 		$name = $_FILES['userfile']['name'];		
 		$name = $directorio.$name;				
 		file_put_contents($name,file_get_contents($tmpName));
+		
+		$obj = new img_opt(); 
+		$obj->max_width(370); 
+		$obj->max_height(363); 
+		$obj->image_path($name); 
+		$obj->image_resize();
 
 if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
   echo "success";
